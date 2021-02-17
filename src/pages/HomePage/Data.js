412f10fceff554e47a1881b45a2de2fb8422a0af
Courtney <crevadaluz@runding.com>
2021-02-17 05:30:16 -0800

    fix: fixes img loading issue for image files
    Use .default to handle an error case in the off chance the image is no
    longer there

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -1,16 +1,18 @@
 export const homeObjOne = {
   lightBg: false,
-  imgStart: "",
-  lineTopLine: true,
-  topLine: "Digital Agency",
-  lightText: true,
-  headline: "lead Generation Specialist for Online Business",
-  description:
-    "We help business owners increase their revenue. Our Team focuses on producing results. We offer unique specialist that can help you achieve your business goals.",
-  lightTextDesc: true,
   primary: true,
+  lightBg: false,
+  imgStart: "",
+  lightTopLine: true,
+  lightTextDesc: true,
   buttonLabel: "Get Started",
-  img: require("../../images/svg-1.svg"),
+  description:
+    "We help business owners increase their revenue.Our Team focuses on producing results. We offer unique specialist that can help you achieve your business goals.",
+  lightTextDesc: true,
+  headline: "Lead Generation Specialist for Online Businesses",
+  lightText: true,
+  topLine: 'Marketing Agency',
+  img: require("../../images/svg-1.svg").default,
   alt: "Image",
   start: "",
 };
