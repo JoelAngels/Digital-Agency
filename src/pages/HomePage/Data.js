334ce8e16dacd1bc836988105dc733c2f3c76e69
JoelAngels <joelbaraka4325@gmail.com>
2021-02-22 09:24:58 +0300

    Added the data js file

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -1,20 +1,16 @@
 export const homeObjOne = {
   lightBg: false,
   primary: true,
-  lightBg: false,
   imgStart: "",
   lightTopLine: true,
   lightTextDesc: true,
   buttonLabel: "Get Started",
   description:
     "We help business owners increase their revenue.Our Team focuses on producing results. We offer unique specialist that can help you achieve your business goals.",
-  lightTextDesc: true,
   headline: "Lead Generation Specialist for Online Businesses",
   lightText: true,
-  topLine: 'Marketing Agency',
+  topLine: "Marketing Agency",
   img: require("../../images/svg-1.svg").default,
   alt: "Image",
   start: "",
 };
-
-export default homeObjOne;
