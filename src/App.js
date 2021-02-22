c48a73f2315931e19912361fee9a657a27892637
JoelAngels <joelbaraka4325@gmail.com>
2021-02-22 13:37:10 +0300

    IMPORTED SOME CODE FORM GITHUB

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import GlobalStyle from "./globalStyles";
 import { Footer, Navbar } from "./components";
 import Home from "./pages/HomePage/Home";
+import Pricing from "./components/Pricing/Pricing";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
       </Switch>
+      <Pricing />
       <Footer />
     </Router>
   );
