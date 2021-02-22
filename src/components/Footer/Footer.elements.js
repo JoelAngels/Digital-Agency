54a1b3f9c14f897013da361c2a9427c7cf19bd22
JoelAngels <joelbaraka4325@gmail.com>
2021-02-22 11:28:59 +0300

    Added the form init

--- src/components/Footer/Footer.elements.js
+++ src/components/Footer/Footer.elements.js
@@ -31,3 +31,33 @@ export const FooterSubText = styled.p`
   margin-bottom: 24px;
   font-size: 20px;
 `;
+
+export const Form = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media screen and (max-width: 820px) {
+    flex-direction: column;
+    width: 80%;
+  }
+`;
+
+export const FormInput = styled.input`
+  padding: 10px 20px;
+  border-radius: 20px;
+  margin-right: 10px;
+  outline: none;
+  border: none;
+  font-size: 16px;
+  border: 1px solid #fff;
+
+  &::placeholder {
+    color: #242424;
+  }
+
+  @media screen and (max-width: 820px) {
+    width: 100%;
+    margin: 0 0 16px 0;
+  }
+`;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Button } from "../../globalStyles";
+
 import {
   FooterContainer,
   FooterSubscription,
   FooterSubHeading,
   FooterSubText,
+  Form,
+  FormInput,
 } from "./Footer.elements";
 
 const Footer = () => {
@@ -15,6 +19,10 @@ const Footer = () => {
             Join our exclusive membership to receive the latest news and trends
           </FooterSubHeading>
           <FooterSubText>You can unsubscribe at any time</FooterSubText>
+          <Form>
+            <FormInput name="email" type="email" placeholder="Your Email" />
+            <Button fontBig>Subscribe</Button>
+          </Form>
         </FooterSubscription>
       </FooterContainer>
     </div>
