86bf05a9301d0bc3d4ed8f543bf983fb1f6259d7
JoelAngels <joelbaraka4325@gmail.com>
2021-02-22 10:05:33 +0300

    Styled the body section but mising ther price section

--- src/components/Navbar/Navbar.Elements.js
+++ src/components/Navbar/Navbar.Elements.js
@@ -11,6 +11,7 @@ export const Nav = styled.nav`
   align-items: center;
   font-size: 1.2rem;
   position: sticky;
+  top: 0;
   z-index: 999; //this goes above everything else
 `;
 
@@ -38,94 +39,94 @@ export const NavIcon = styled(FaMagento)`
 `;
 
 export const MobileIcon = styled.div`
-display: none;
+  display: none;
 
-@media screen and (max-width: 960px) {
-  display: block;
-  position: absolute;
-  top: 0;
-  right: 0;
-  transform: translate(-100%, 60%);
-  font-size: 1.8rem;
-  cursor: pointer;
-}
+  @media screen and (max-width: 960px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
 `;
 
-export const NavMenu  = styled.ul`
-display: flex;
-align-items: center;
-list-style: none;
-text-align: center;
-
-@media screen and (max-width: 960px) {
+export const NavMenu = styled.ul`
   display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 90vh;
-  position: absolute;
-  top: 80px;
-  left: ${({click}) => (click ? 0 : "-100%")};
-  opacity: 1;
-  transition: all 0.5s ease;
-  background: #101522;
-}
+  align-items: center;
+  list-style: none;
+  text-align: center;
+
+  @media screen and (max-width: 960px) {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 90vh;
+    position: absolute;
+    top: 80px;
+    left: ${({ click }) => (click ? 0 : "-100%")};
+    opacity: 1;
+    transition: all 0.5s ease;
+    background: #101522;
+  }
 `;
 
 export const NavItem = styled.li`
-height: 80px;
-border-bottom: 2px solid transparent;
+  height: 80px;
+  border-bottom: 2px solid transparent;
 
-&:hover {
-  border-bottom: 2px solid #4b59f7;
-}
-
-@media screen and (max-width: 960px) {
-  width: 100%;
   &:hover {
-    border: none;
+    border-bottom: 2px solid #4b59f7;
   }
-}
+
+  @media screen and (max-width: 960px) {
+    width: 100%;
+    &:hover {
+      border: none;
+    }
+  }
 `;
 
 export const NavLinks = styled(Link)`
-color: #fff;
-display: flex;
-align-items: center;
-text-decoration: none;
-padding: 0.5rem 1rem;
-height: 100%;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0.5rem 1rem;
+  height: 100%;
 
-@media screen and (max-width: 960px) {
-  text-align: center;
-  padding: 2rem;
-  width: 100%;
-  display: table;
-}
+  @media screen and (max-width: 960px) {
+    text-align: center;
+    padding: 2rem;
+    width: 100%;
+    display: table;
+  }
 
-&:hover {
-  color: #4b59f7;
-  transition: all 0.3s ease;
-}
+  &:hover {
+    color: #4b59f7;
+    transition: all 0.3s ease;
+  }
 `;
 
 export const NavItemBtn = styled.li`
-@media screen and (max-width: 960px) {
-  display: flex;
-justify-content: center;
-align-items: center;
-width: 100%;
-height: 120px;
-}
+  @media screen and (max-width: 960px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 120px;
+  }
 `;
 
 export const NavBtnLink = styled(Link)`
-display: flex;
-justify-content: center;
-align-items: center;
-text-decoration: none;
-padding: 8px 16px;
-height: 100%;
-width: 100%;
-border: none;
-outline: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  padding: 8px 16px;
+  height: 100%;
+  width: 100%;
+  border: none;
+  outline: none;
 `;
