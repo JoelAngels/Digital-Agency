ac30387ac02c98b75dc63b2a2a0af8bea64c7dbf
JoelAngels <joelbaraka4325@gmail.com>
2021-02-22 12:27:22 +0300

    Created the footer design

--- src/components/Footer/Footer.elements.js
+++ src/components/Footer/Footer.elements.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const FooterContainer = styled.div`
   background-color: #101522;
@@ -22,7 +23,7 @@ export const FooterSubscription = styled.section`
 
 export const FooterSubHeading = styled.p`
   font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
-    "Lucide Sans", Arial, sans-serif, sans-serif;
+    "Lucida Sans", Arial, sans-serif;
   margin-bottom: 24px;
   font-size: 24px;
 `;
@@ -30,4 +31,85 @@ export const FooterSubHeading = styled.p`
 export const FooterSubText = styled.p`
   margin-bottom: 24px;
   font-size: 20px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media screen and (max-width: 820px) {
+    flex-direction: column;
+    width: 80%;
+  }
+`;
+
+export const FormInput = styled.input`
+  padding: 10px 20px;
+  border-radius: 2px;
+  margin-right: 10px;
+  outline: none;
+  border: none;
+  font-size: 16px;
+  border: 1px solid #fff;
+
+  &::placeholder {
+    color: #242424;
+  }
+
+  @media screen and (max-width: 820px) {
+    width: 100%;
+    margin: 0 0 16px 0;
+  }
+`;
+
+export const FooterLinksContainer = styled.div`
+  width: 100%;
+  max-width: 1000px;
+  display: flex;
+  justify-content: center;
+
+  @media screen and (max-width: 820px) {
+    padding-top: 32px;
+  }
+`;
+
+export const FooterLinksWrapper = styled.div`
+  display: flex;
+
+  @media screen and (max-width: 820px) {
+    flex-direction: column;
+  }
+`;
+
+export const FooterLinksItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 16px;
+  text-align: left;
+  width: 160px;
+  box-sizing: border-box;
+  color: #fff;
+
+  @media screen and (max-width: 420px) {
+    margin: 0;
+    padding: 10px;
+    width: 100%;
+  }
+`;
+
+export const FooterLinkTitle = styled.h2`
+  margin-bottom: 16px;
+`;
+
+export const FooterLink = styled(Link)`
+  color: #fff;
+  text-decoration: none;
+  margin-bottom: 0.5rem;
+
+  &:hover {
+    color: #0467fb;
+    transition: 0.3s ease-out;
+  }
 `;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Button } from "../../globalStyles";
+
 import {
   FooterContainer,
   FooterSubscription,
   FooterSubHeading,
   FooterSubText,
+  Form,
+  FormInput,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinksItems,
+  FooterLinkTitle,
+  FooterLink,
 } from "./Footer.elements";
 
 const Footer = () => {
@@ -15,7 +24,43 @@ const Footer = () => {
             Join our exclusive membership to receive the latest news and trends
           </FooterSubHeading>
           <FooterSubText>You can unsubscribe at any time</FooterSubText>
+          <Form>
+            <FormInput name="email" type="email" placeholder="Your Email" />
+            <Button fontBig>Subscribe</Button>
+          </Form>
         </FooterSubscription>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinksItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to="/sign-up">How it works</FooterLink>
+              <FooterLink to="/">Testimonials</FooterLink>
+              <FooterLink to="/">Careers</FooterLink>
+              <FooterLink to="/">Terms of Service</FooterLink>
+            </FooterLinksItems>
+            <FooterLinksItems>
+              <FooterLinkTitle>Contact Us</FooterLinkTitle>
+              <FooterLink to="/sign-up">How it works</FooterLink>
+              <FooterLink to="/">Testimonials</FooterLink>
+              <FooterLink to="/">Careers</FooterLink>
+              <FooterLink to="/">Terms of Service</FooterLink>
+            </FooterLinksItems>
+            <FooterLinksItems>
+              <FooterLinkTitle>Videos</FooterLinkTitle>
+              <FooterLink to="/sign-up">How it works</FooterLink>
+              <FooterLink to="/">Testimonials</FooterLink>
+              <FooterLink to="/">Careers</FooterLink>
+              <FooterLink to="/">Terms of Service</FooterLink>
+            </FooterLinksItems>
+            <FooterLinksItems>
+              <FooterLinkTitle>Social Media</FooterLinkTitle>
+              <FooterLink to="/sign-up">How it works</FooterLink>
+              <FooterLink to="/">Testimonials</FooterLink>
+              <FooterLink to="/">Careers</FooterLink>
+              <FooterLink to="/">Terms of Service</FooterLink>
+            </FooterLinksItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
       </FooterContainer>
     </div>
   );
