8bed2f99b81cfd208861f3310c76715e42e8d67d
Joel Angel <joelbaraka4325@gmail.com>
2023-09-14 00:18:44 +0300

    fixed bulld issue

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import GlobalStyle from "./globalStyles";
 import { Footer, Navbar } from "./components";
